22ece642addadd86e89ed868aeca552c5a54a8a1
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2021-07-01 07:44:53 +0300

    Update and rename Simon1.2.R to PKD3.py

--- Simon1.2.R
+++
@@ -1,5 +0,0 @@
-par(mfrow=c(5,4))                         
-# in the expressions below, ~ is a space and == is equal
-plot(arima.sim(list(order=c(1,0,0), ar=.9), n=100), ylab="x", main=(expression(AR(1)~~~phi==+.9))) 
-plot(arima.sim(list(order=c(1,0,0), ar=-.9), n=100), ylab="x", main=(expression(AR(1)~~~phi==-.9)))
-
