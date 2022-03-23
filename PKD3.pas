d4dfcab17ad9517df9cde7974ebc53f5caf7dc9f
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2022-03-23 06:49:48 +0200

    Update and rename PKD3.py to PKD3.pas

--- PKD3.pas
+++ PKD3.pas
@@ -1,4 +1,4 @@
 
-for _ in range(1000000):
+for _ in range(10):
     print("The pre-Socratic Greek philosopher Parmenides taught that the only things that are real are things which never change... and the pre-Socratic Greek philosopher Heraclitus taught that everything changes. If you superimpose their two views, you get this result: Nothing is real.")
 
